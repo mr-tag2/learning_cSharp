e2a4eb3c5945f2104f38b978468e8bdd2ab6f9f6
mr_tag2 <104317822+mr-tag2@users.noreply.github.com>
2024-12-13 11:00:14 +0330

    ..

---
+++ q3-2/q3-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Q3_2
+{
+    internal class Program
+    {
+
+        static Thread[] threads = new Thread[4];
+        static int step = 0;
+        static string[] arrayWord = [
+            "young",
+            "lion",
+            "are",
+            "fast"
+            ];
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int j = i;
+                threads[j] = new Thread(() =>
+                {
+                    while (step < 4)
+                        if (step < 4 && j == step)
+                        {
+                            Console.Write(arrayWord[j]+" ");
+                            step++;
+                        }
+
+                });
+                threads[j].Start();
+            }
+        }
+
+
+    }
+
+}
